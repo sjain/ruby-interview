112bc40f8f9815f9108ca9ab59c287e4671918ae
Sharad Jain <shaanjain@gmail.com>
2017-01-25 15:56:25 -0600

    added a few more scenario's to build up the ruby logic using TDD

--- coins.rb
+++ coins.rb
@@ -3,9 +3,19 @@ require 'rspec'
 # TODO Define a CoinCounter class to count coins
 
 describe 'a scenario' do
-  it 'calculates value' do
+  it 'calculates total value of coins' do
     cc = CoinCounter.new([25,5,1,10])
     expect(cc.value).to eq(25+5+1+10)
   end   
+
+  # it 'calculates total value of coins even if a coin is specified as string' do
+  #   cc = CoinCounter.new(["25",5,1,10])
+  #   expect(cc.value).to eq(25+5+1+10)
+  # end
+
+  # it 'fails fast when an invalid coin is given' do
+  #   cc = CoinCounter.new(["mickey mouse",5,1,10])
+  #   fail('TODO assert failure here') # What's the best way to fail fast?
+  # end
 end
 
